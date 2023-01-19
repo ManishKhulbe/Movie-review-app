71c58f1ce39a0b4bc79084e594831589a2a882b8
mobcoder-manish-khulbe <mobcoder-manish-khulbe>
2023-01-19 12:06:34 +0530

    initial commit

---
+++ moviefrontend/src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./components/user/Navbar";
+
+function app() {
+  return <Navbar />;
+}
+
+export default app;
---
+++ moviefrontend/src/components/user/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-red-200">
+      <div className="bg-black text-white max-w-screen-xl mx-auto">app</div>
+    </div>
+  );
+};
+
+export default Navbar;

814bce7d4d6f43f5e49af65fb2d0e7963c071a81
manishkhulbe123@gmail.com <manishkhulbe123@gmail.com>
2023-03-12 20:23:10 +0530

    working on cast form

--- moviefrontend/src/components/form/CastForm.jsx
+++ moviefrontend/src/components/form/CastForm.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import LiveSearch from "../admin/LiveSearch";
 
+const defaultCastInfo = {
+  profile: {},
+  roleAs: "",
+  leadActor: false,
+};
 const CastForm = () => {
-    
+  const [castInfo, setCastInfo] = useState({ ...defaultCastInfo });
+  const { leadActor } = castInfo;
   return (
-    <div>
-      
+    <div className="flex items-center space-x-2">
+      <input
+        type="checkbox"
+        name="leadActor"
+        className="w-4 h-4"
+        checked={leadActor}
+      />
+      <LiveSearch placeholder="Search profile.."/>
     </div>
-  )
-}
+  );
+};
 
-export default CastForm
+export default CastForm;
